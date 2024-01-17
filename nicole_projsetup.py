45c845371b6eb7373ffb13989c3cf27e92e7922e
nhansen23 <156024214+nhansen23@users.noreply.github.com>
2024-01-16 18:29:50 -0600

    Create nicole_projsetup.py

---
+++ nicole_projsetup.py
@@ -0,0 +1,20 @@
+''' This module is used to practice looping and branching.  It provides functions for creating a series of project folders. '''
+
+import math
+import statistics
+# import module from previous project: datafun-01-attr
+import maverick_analytics_utils 
+
+# Function 1: generate folders for a given range
+def create_folders_range(start_year, end_year):
+
+# Function 2: generate folders from a list
+def create_folders_list(folder_list):
+
+#Function 3: generate prefixed folders by transforming a list of names and combining with a prefix
+def create_prefixed_folders(folder_list, prefix):
+
+
+# Function 4: create folders with a while loop
+def create_folders_periodically(duration):
+  
